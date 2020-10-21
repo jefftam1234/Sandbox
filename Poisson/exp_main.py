f824ad4d44619b3307ac61cab1fd31f5146aa97d
jefftam1234 <36940766+jefftam1234@users.noreply.github.com>
2020-10-22 00:42:15 +0900

    Add: poisson simulation with exponential scheme

--- Poisson/exp_main.py
+++ Poisson/exp_main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-credit_spread = 0.10
+credit_spread = 0.1
 T = 4.1
 RecovRate = 0.4
 path = 500000
@@ -17,19 +17,19 @@ prob_def = [1-math.exp(-credit_spread/(1-RecovRate) * i) for i in dt]
 
 for n in range(iter):
     default_matrix = np.zeros((path, len(dt)), int)
-    uni_var = np.random.uniform(0,1,path*len(dt))
+    uni_var = np.random.uniform(0,1,path)
+    exp_var = [-math.log(1-uv) for uv in uni_var]
     default_path = np.zeros(path, int)
 
     for i in range(path):
         ruin = False
+        accum_haz = 0.0
         for j in range(len(dt)):
-            if ruin:
-                continue
-            else:
-                nIndex = i * len(dt) + j
-                if uni_var[nIndex] < prob_def[j]:
-                    default_matrix[i, j] = 1
-                    default_path[i] = 1
+            accum_haz += prob_def[j]
+            if accum_haz >= exp_var[i]:
+                ruin = True
+                default_matrix[i, j] = 1
+                default_path[i] = 1
 
     theo_default = 1-math.exp(-credit_spread/(1-RecovRate)*T)
     mc_default = sum(default_path)/path
